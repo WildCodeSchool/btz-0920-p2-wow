c474a8321c37b10f782f41ab1e9b3be2a49e413f
Karnak19 <basile64.v@gmail.com>
2020-10-26 09:35:28 +0100

    init :fire:

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
