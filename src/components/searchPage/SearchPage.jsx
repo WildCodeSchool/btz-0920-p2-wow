c5eff94541050e23b6c9882f7b54f28f3fa79106
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-27 11:59:36 +0100

    added searchPage (jsx+css) + wildCard components + add 2 pics (char&guild)

---
+++ src/components/searchPage/SearchPage.jsx
@@ -0,0 +1,14 @@
+// import React, { Component } from 'react';
+import './SearchPage.css';
+import WildCard from './WildCard';
+
+const SearchPage = () => {
+  return (
+    <div className="container">
+      <h1 className="border-bot-primary title-y">Looking For</h1>
+      <WildCard />
+    </div>
+  );
+};
+
+export default SearchPage;
---
+++ src/components/searchPage/WildCard.jsx
@@ -0,0 +1,14 @@
+import './SearchPage.css';
+
+const WildCard = () => {
+  return (
+    <div
+      className="wildCard"
+      style={{ backgroundImage: '../img/character.jpg' }}
+    >
+      <h3>Title</h3>
+    </div>
+  );
+};
+
+export default WildCard;
