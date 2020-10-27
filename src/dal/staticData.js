d411d7c51f29767e35ef42dfdd81ad09ed91d9f7
DavidDvpt <david.mosca69@gmail.com>
2020-10-27 11:42:22 +0100

    adding arrays classes, faction, regions, roles and raid to static data

---
+++ src/dal/staticData.js
@@ -0,0 +1,190 @@
+/**
+ * Static arrays.
+ * all arrays contains objects with id, name and slug
+ * created on 27-10-2020
+ */
+
+/**
+ * array of region
+ */
+export const regions = [
+  {
+    id: 1,
+    name: 'United States & Oceania',
+    slug: 'us',
+  },
+  {
+    id: 2,
+    name: 'Europe',
+    slug: 'eu',
+  },
+  {
+    id: 3,
+    name: 'Korean',
+    slug: 'kr',
+  },
+  {
+    id: 4,
+    name: 'Taiwan',
+    slug: 'tw',
+  },
+];
+
+/**
+ * array of factions
+ */
+export const factions = [
+  {
+    id: 1,
+    name: 'Alliance',
+    slug: 'alliance',
+  },
+  {
+    id: 2,
+    name: 'Horde',
+    slug: 'horde',
+  },
+];
+
+/**
+ * array of classes
+ */
+export const classes = [
+  {
+    id: 1,
+    name: 'Death Knight',
+    slug: 'death-knight',
+  },
+  {
+    id: 2,
+    name: 'Demon Hunter',
+    slug: 'demon-hunter',
+  },
+  {
+    id: 3,
+    name: 'Druid',
+    slug: 'druid',
+  },
+  {
+    id: 4,
+    name: 'Alliance',
+    slug: 'hunter',
+  },
+  {
+    id: 5,
+    name: 'Mage',
+    slug: 'mage',
+  },
+  {
+    id: 6,
+    name: 'Monk',
+    slug: 'monk',
+  },
+  {
+    id: 7,
+    name: 'Paladin',
+    slug: 'paladin',
+  },
+  {
+    id: 8,
+    name: 'Priest',
+    slug: 'priest',
+  },
+  {
+    id: 9,
+    name: 'Rogue',
+    slug: 'rogue',
+  },
+  {
+    id: 10,
+    name: 'Shaman',
+    slug: 'shaman',
+  },
+  {
+    id: 11,
+    name: 'Warlock',
+    slug: 'warlock',
+  },
+  {
+    id: 12,
+    name: 'Warrior',
+    slug: 'warrior',
+  },
+];
+
+/**
+ * array of roles
+ */
+export const roles = [
+  {
+    id: 1,
+    name: 'Tank',
+    slug: 'tank',
+  },
+  {
+    id: 1,
+    name: 'DPS',
+    slug: 'dps',
+  },
+  {
+    id: 1,
+    name: 'Healer',
+    slug: 'healer',
+  },
+];
+
+/**
+ * array of raids
+ */
+export const raids = [
+  {
+    id: 1,
+    name: "Ny'alotha, the Waking City",
+    slug: 'nyalotha-the-waking-city',
+  },
+  {
+    id: 2,
+    name: 'The Eternal Palace',
+    slug: 'the-eternal-palace',
+  },
+  {
+    id: 3,
+    name: 'Crucible of Storms',
+    slug: 'crucible-of-storms',
+  },
+  {
+    id: 4,
+    name: "Battle of Dazar'alor",
+    slug: 'battle-of-dazaralor',
+  },
+  {
+    id: 5,
+    name: 'Uldir',
+    slug: 'uldir',
+  },
+  {
+    id: 6,
+    name: 'Antorus, the Burning Throne',
+    slug: 'antonus-the-burning-throne',
+  },
+  {
+    id: 7,
+    name: 'Tomb of Sargeras',
+    slug: 'tomb-of-sargeras',
+  },
+  {
+    id: 8,
+    name: 'The Nighthold',
+    slug: 'the-nighthold',
+  },
+  {
+    id: 9,
+    name: 'Trial of Valor',
+    slug: 'trial-of-valor',
+  },
+  {
+    id: 10,
+    name: 'The Emerald Nightmare',
+    slug: 'the-emerald-nightmare',
+  },
+];
