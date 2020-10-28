470d9e00333eda7da258b6db3ea1af211db0710f
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-10-28 15:13:29 +0100

    created PlayerProfile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line import/no-named-as-default-member
+import PlayerProfile from './components/PlayerProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlayerProfile />
     </div>
   );
 }
---
+++ src/components/PlayerProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+
+class PlayerProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <img src="" alt="" />
+        <Table />
+      </div>
+    );
+  }
+}
+
+export default PlayerProfile;
