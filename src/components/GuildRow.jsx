12027ffebe2b1519322809f7f1bb7c3d76c37bc4
RZucconi <romain.zucconi@gmail.com>
2020-10-30 16:29:28 +0100

    delete realm nationality display horde or alliance flag

--- src/components/GuildRow.jsx
+++ src/components/GuildRow.jsx
@@ -1,25 +1,22 @@
 import PropTypes from 'prop-types';
 import { GiWorld } from 'react-icons/gi';
 
-import EuroFlag from './EuroFlag';
-import ChinaFlag from './ChinaFlag';
-import UsFlag from './UsFlag';
+import HordeFlag from './HordeFlag';
+import AllianceFlag from './AllianceFlag';
 
-const flag = (slug) => {
-  switch (slug) {
-    case 'eu':
-      return <EuroFlag />;
-    case 'us':
-      return <UsFlag />;
-    case 'ch':
-      return <ChinaFlag />;
+const flag = (faction) => {
+  switch (faction) {
+    case 'horde':
+      return <HordeFlag />;
+    case 'alliance':
+      return <AllianceFlag />;
     default:
       return <GiWorld />;
   }
 };
 
 function GuildRow(props) {
-  const { name, faction, slug, rank, bckgrdStyle } = props;
+  const { name, faction, rank, bckgrdStyle } = props;
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <tr {...bckgrdStyle}>
@@ -27,10 +24,7 @@ function GuildRow(props) {
       <th>
         <strong style={{ fontSize: '21px' }}>{name}</strong>
       </th>
-      <td>
-        <p>{faction}</p>
-      </td>
-      <td>{flag(slug)}</td>
+      <td>{flag(faction)}</td>
     </tr>
   );
 }
