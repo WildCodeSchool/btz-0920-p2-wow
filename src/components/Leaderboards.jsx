4374a5e6d763b1a0eb2dbe9c8155bb49d844e865
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-10-31 12:42:45 +0100

    refactored players leaderboard by adding a map method

--- src/components/Leaderboards.jsx
+++ src/components/Leaderboards.jsx
@@ -3,6 +3,7 @@ import { Table } from 'reactstrap';
 import DalApi from '../dal/DalApi';
 import LoadingSpinner from './LoadingSpinner';
 import GuildLeaderboardRow from './GuildLeaderboardRow';
+import PlayerLeaderboardRow from './PlayerLeaderboardRow';
 
 class Leaderboards extends React.Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class Leaderboards extends React.Component {
     this.state = {
       guildResults: [],
       loadingGuilds: true,
-      // loadingPlayers: true,
+      loadingPlayers: true,
     };
   }
 
@@ -21,64 +22,25 @@ class Leaderboards extends React.Component {
         loadingGuilds: false,
       });
     });
-    // DalApi.getTopPlayer((data) => {
-    //   const { rankings } = data;
-    //   this.setState({
-    //     firstPlayerName: rankings.rankedCharacters[0].character.name,
-    //     secondPlayerName: rankings.rankedCharacters[1].character.name,
-    //     thirdPlayerName: rankings.rankedCharacters[2].character.name,
-    //     fourthPlayerName: rankings.rankedCharacters[3].character.name,
-    //     fifthPlayerName: rankings.rankedCharacters[4].character.name,
-    //     firstPlayerRealm:
-    //       data.rankings.rankedCharacters[0].character.realm.name,
-    //     secondPlayerRealm:
-    //       data.rankings.rankedCharacters[1].character.realm.name,
-    //     thirdPlayerRealm:
-    //       data.rankings.rankedCharacters[2].character.realm.name,
-    //     fourthPlayerRealm:
-    //       data.rankings.rankedCharacters[3].character.realm.name,
-    //     fifthPlayerRealm:
-    //       data.rankings.rankedCharacters[4].character.realm.name,
-    //     firstPlayerRegion:
-    //       data.rankings.rankedCharacters[0].character.region.name,
-    //     secondPlayerRegion:
-    //       data.rankings.rankedCharacters[1].character.region.name,
-    //     thirdPlayerRegion:
-    //       data.rankings.rankedCharacters[2].character.region.name,
-    //     fourthPlayerRegion:
-    //       data.rankings.rankedCharacters[3].character.region.name,
-    //     fifthPlayerRegion:
-    //       data.rankings.rankedCharacters[4].character.region.name,
-    //     loadingPlayers: false,
-    //   });
-    // });
+    DalApi.getTopPlayer((data) => {
+      this.setState({
+        playerResults: data.rankings.rankedCharacters,
+        loadingPlayers: false,
+      });
+    });
   }
 
   render() {
     const {
       guildResults,
-      // firstPlayerName,
-      // secondPlayerName,
-      // thirdPlayerName,
-      // fourthPlayerName,
-      // fifthPlayerName,
-      // firstPlayerRealm,
-      // secondPlayerRealm,
-      // thirdPlayerRealm,
-      // fourthPlayerRealm,
-      // fifthPlayerRealm,
-      // firstPlayerRegion,
-      // secondPlayerRegion,
-      // thirdPlayerRegion,
-      // fourthPlayerRegion,
-      // fifthPlayerRegion,
+      playerResults,
       loadingGuilds,
-      // loadingPlayers,
+      loadingPlayers,
     } = this.state;
     return (
       <div>
         <div className="leaderboard-container container d-flex mt-5 justify-content-center">
-          {loadingGuilds ? (
+          {loadingGuilds || loadingPlayers ? (
             <LoadingSpinner />
           ) : (
             <div className="d-flex mt-5 align-items-center justify-content-center">
@@ -104,7 +66,7 @@ class Leaderboards extends React.Component {
                     .filter((_, index) => index < 5)}
                 </tbody>
               </Table>
-              {/* <Table className="mx-5" hover>
+              <Table className="mx-5" hover>
                 <thead>
                   <tr>
                     <th className="h2 font-weight-bold col-md-6">
@@ -113,33 +75,19 @@ class Leaderboards extends React.Component {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr className="bg-primary">
-                    <th>{firstPlayerName}</th>
-                    <th>{firstPlayerRealm}</th>
-                    <th>{displaysFlag(firstPlayerRegion)}</th>
-                  </tr>
-                  <tr>
-                    <th>{secondPlayerName}</th>
-                    <th>{secondPlayerRealm}</th>
-                    <th>{displaysFlag(secondPlayerRegion)}</th>
-                  </tr>
-                  <tr>
-                    <th>{thirdPlayerName}</th>
-                    <th>{thirdPlayerRealm}</th>
-                    <th>{displaysFlag(thirdPlayerRegion)}</th>
-                  </tr>
-                  <tr>
-                    <th>{fourthPlayerName}</th>
-                    <th>{fourthPlayerRealm}</th>
-                    <th>{displaysFlag(fourthPlayerRegion)}</th>
-                  </tr>
-                  <tr>
-                    <th>{fifthPlayerName}</th>
-                    <th>{fifthPlayerRealm}</th>
-                    <th>{displaysFlag(fifthPlayerRegion)}</th>
-                  </tr>
+                  {playerResults
+                    .map((result) => {
+                      return (
+                        <PlayerLeaderboardRow
+                          name={result.character.name}
+                          realm={result.character.realm.name}
+                          region={result.character.region.name}
+                        />
+                      );
+                    })
+                    .filter((_, index) => index < 5)}
                 </tbody>
-              </Table> */}
+              </Table>
             </div>
           )}
         </div>
