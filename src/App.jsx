04c56debe4bb282f56ebdc83a1bb06c63d64ecf8
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-10-31 10:01:01 +0100

    added router to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import GuildPage from './components/guildPage';
 import PlayerProfile from './components/PlayerProfile';
-// import Leaderboards from './components/Leaderboards';
+import Leaderboards from './components/Leaderboards';
 import NavBar from './components/NavBar';
 import GuildsArray from './components/GuildsArray';
-import Leaderboards from './components/Leaderboards';
-// import GuildPage from './components/guildPage';
-// import SearchPage from './components/searchPage/SearchPage';
+import GuildPage from './components/guildPage';
+import SearchPage from './components/searchPage/SearchPage';
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        fontFamily: 'Ubuntu',
-        fontSize: '48px',
-        fontStyle: 'normal',
-        fontWeight: '700',
-        lineHeight: '55px',
-        letterSpacing: '0em',
-        textAlign: 'center',
-      }}
-    >
-      <NavBar />
-      <PlayerProfile />
-      <GuildsArray />
-      {/* <GuildPage /> */}
-      <Leaderboards />
-      {/* <SearchPage /> */}
-    </div>
+    <Router>
+      <div
+        className="App"
+        style={{
+          fontFamily: 'Ubuntu',
+          fontSize: '48px',
+          fontStyle: 'normal',
+          fontWeight: '700',
+          lineHeight: '55px',
+          letterSpacing: '0em',
+          textAlign: 'center',
+        }}
+      >
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Leaderboards} />
+          <Route path="/PlayerProfile" component={PlayerProfile} />
+          <Route path="/GuildsArray" component={GuildsArray} />
+          <Route path="/GuildPage" component={GuildPage} />
+          <Route path="/SearchPage" component={SearchPage} />
+          {/* <PlayerProfile /> */}
+          {/* <GuildsArray /> */}
+          {/* <GuildPage /> */}
+          {/* <Leaderboards /> */}
+          {/* <SearchPage /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
