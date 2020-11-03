abde50ed826ea21e1d9eac71f0e7fbfefaef7fe0
RZucconi <romain.zucconi@gmail.com>
2020-11-03 22:50:28 +0100

    using hooks on GuildsArray and PJArray component

--- src/components/GuildsArray.jsx
+++ src/components/GuildsArray.jsx
@@ -1,6 +1,7 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Table, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import { BsFillSkipBackwardFill, BsFillSkipForwardFill } from 'react-icons/bs';
+// import { useParams } from 'react-router-dom';
 
 import GuildRow from './GuildRow';
 import ToolsFilters from './ToolsFilters';
@@ -10,138 +11,118 @@ import Hr from './cssPages&Components/Hr';
 import LoadingSpinner from './LoadingSpinner';
 import './cssPages&Components/GuildsArray.css';
 
-class GuildsArray extends Component {
-  constructor() {
-    super();
-    this.state = {
-      results: [],
-      serverSlug: '',
-      realmName: '',
-      loadingGuilds: true,
-      min: 0,
-      max: 5,
-    };
-    this.pageUp = this.pageUp.bind(this);
-    this.pageDown = this.pageDown.bind(this);
-    this.page1 = this.page1.bind(this);
-    this.page2 = this.page2.bind(this);
-    this.page3 = this.page3.bind(this);
-    this.page4 = this.page4.bind(this);
-  }
+const GuildsArray = () => {
+  // const params = useParams();
+  const [results, setResults] = useState([]);
+  const [loadingGuilds, setLoadingGuilds] = useState(true);
+  const [serverSlug, setServerSlug] = useState('');
+  const [realmName, setRealmName] = useState('');
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(5);
 
-  componentDidMount() {
+  useEffect(() => {
     DalApi.getTopGuild(
       (guildsRes) => {
-        this.setState({
-          results: guildsRes.raidRankings.rankedGuilds,
-          serverSlug: guildsRes.raidRankings.rankedGuilds[0].guild.region.slug,
-          realmName: guildsRes.raidRankings.rankedGuilds[0].guild.realm.name,
-          loadingGuilds: false,
-        });
+        const { rankedGuilds } = guildsRes.raidRankings;
+        setResults(rankedGuilds);
+        setServerSlug(rankedGuilds[0].guild.region.slug);
+        setRealmName(rankedGuilds[0].guild.realm.name);
+        setLoadingGuilds(false);
       },
+      // params.region,
+      // params.realm,
+      // params.name
       'us',
       "Mal'ganis"
     );
+  }, []);
+
+  function page1() {
+    setMin(0);
+    setMax(5);
   }
 
-  pageUp() {
-    const { min, max } = this.state;
-    this.setState({ min: min + 5, max: max + 5 });
+  function page2() {
+    setMin(5);
+    setMax(10);
   }
 
-  pageDown() {
-    const { min, max } = this.state;
-    this.setState({ min: min - 5, max: max - 5 });
+  function page3() {
+    setMin(10);
+    setMax(15);
   }
 
-  page1() {
-    this.setState({ min: 0, max: 5 });
+  function page4() {
+    setMin(15);
+    setMax(20);
   }
 
-  page2() {
-    this.setState({ min: 5, max: 10 });
-  }
-
-  page3() {
-    this.setState({ min: 10, max: 15 });
-  }
-
-  page4() {
-    this.setState({ min: 15, max: 20 });
-  }
-
-  render() {
-    const {
-      results,
-      serverSlug,
-      realmName,
-      loadingGuilds,
-      min,
-      max,
-    } = this.state;
-
-    if (loadingGuilds) return <LoadingSpinner />;
-    return (
-      <div className="cssStyle d-flex flex-column text-center">
-        <div>
-          <h2>
-            Top <span>{serverSlug}</span> {realmName} Guilds
-          </h2>
-          <Hr />
-        </div>
-        <main className="container min-vw-100">
-          <div className="row w-100">
-            <div className="col-1 align-self-center">
-              <ToolsFilters />
-            </div>
-            <Table className="col-10" w-auto text-nowrap hover>
-              <tbody className="container">
-                {results
-                  .map((result) => {
-                    return (
-                      <GuildRow
-                        name={result.guild.name}
-                        faction={result.guild.faction}
-                        slug={result.guild.region.slug}
-                        rank={result.rank}
-                        region={result.guild.region.name}
-                        realm={result.guild.realm.name}
-                        key={result.rank}
-                      />
-                    );
-                  })
-                  .filter((_, index) => index >= min && index < max)}
-              </tbody>
-            </Table>
+  return (
+    <>
+      {loadingGuilds ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="cssStyle d-flex flex-column text-center">
+          <div>
+            <h2>
+              Top <span>{serverSlug}</span> {realmName} Guilds
+            </h2>
+            <Hr />
           </div>
-        </main>
-        <Pagination className="align-self-center" size="lg clearfix">
-          <PaginationItem className="paginationItem">
-            <PaginationLink onClick={this.page1}>
-              <BsFillSkipBackwardFill />
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem className="paginationItem">
-            <PaginationLink onClick={this.page1}>1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem className="paginationItem">
-            <PaginationLink onClick={this.page2}>2</PaginationLink>
-          </PaginationItem>
-          <PaginationItem className="paginationItem">
-            <PaginationLink onClick={this.page3}>3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem className="paginationItem">
-            <PaginationLink onClick={this.page4}>4</PaginationLink>
-          </PaginationItem>
-          <PaginationItem className="paginationItem">
-            <PaginationLink onClick={this.page4}>
-              <BsFillSkipForwardFill />
-            </PaginationLink>
-          </PaginationItem>
-        </Pagination>
-      </div>
-    );
-  }
-}
+          <main className="container min-vw-100">
+            <div className="row w-100">
+              <div className="col-1 align-self-center">
+                <ToolsFilters />
+              </div>
+              <Table className="col-10" w-auto text-nowrap hover>
+                <tbody className="container">
+                  {results
+                    .map((result) => {
+                      return (
+                        <GuildRow
+                          name={result.guild.name}
+                          faction={result.guild.faction}
+                          slug={result.guild.region.slug}
+                          rank={result.rank}
+                          region={result.guild.region.name}
+                          realm={result.guild.realm.name}
+                          key={result.rank}
+                        />
+                      );
+                    })
+                    .filter((_, index) => index >= min && index < max)}
+                </tbody>
+              </Table>
+            </div>
+          </main>
+          <Pagination className="align-self-center" size="lg clearfix">
+            <PaginationItem className="paginationItem">
+              <PaginationLink onClick={page1}>
+                <BsFillSkipBackwardFill />
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem className="paginationItem">
+              <PaginationLink onClick={page1}>1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem className="paginationItem">
+              <PaginationLink onClick={page2}>2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem className="paginationItem">
+              <PaginationLink onClick={page3}>3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem className="paginationItem">
+              <PaginationLink onClick={page4}>4</PaginationLink>
+            </PaginationItem>
+            <PaginationItem className="paginationItem">
+              <PaginationLink onClick={page4}>
+                <BsFillSkipForwardFill />
+              </PaginationLink>
+            </PaginationItem>
+          </Pagination>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default GuildsArray;
