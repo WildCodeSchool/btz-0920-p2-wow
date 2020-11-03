b8d0075571bfdd53dd945cb244afd1e521d442d4
a-dagnicourt <dagnicourt.a@gmail.com>
2020-11-03 11:57:07 +0100

    refactored searchpage destructuring carousel sliders+ images handling

---
+++ src/img/index.js
@@ -0,0 +1,25 @@
+import guild from './guild.jpg';
+import alliance from './alliance.png';
+import horde from './horde.png';
+import character from './character.jpg';
+import europe from './europe.png';
+import usa from './usa.png';
+import korea from './korea.png';
+import taiwan from './taiwan.png';
+
+// const images = [
+//   {
+//     id: 0,
+//     src: './guild.jpg',
+//     title: 'guild',
+//     description: 'Wow guild members defeating a Boss',
+//   },
+//   {
+//     id: 1,
+//     src: './character.jpg',
+//     title: 'character',
+//     description: 'Wow characters',
+//   },
+// ];
+
+export { guild, alliance, horde, character, europe, usa, korea, taiwan };
