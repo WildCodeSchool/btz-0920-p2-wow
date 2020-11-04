8b216617b613a95a49f9de5200e3f79c9fa41330
RZucconi <romain.zucconi@gmail.com>
2020-11-04 12:18:04 +0100

    change nav logo

---
+++ src/components/flags/WoWildCraftLogo.jsx
@@ -0,0 +1,9 @@
+import WoWildCraftLog from '../../img/WoWildCraftLogo.png';
+
+function WoWildCraft() {
+  return (
+    <img src={WoWildCraftLog} alt="World of WildCraft Logo" height="200px" />
+  );
+}
+
+export default WoWildCraft;
