009ee2c87fece9ed8495a1e8e1a1b6d9529c3e32
DavidDvpt <david.mosca69@gmail.com>
2020-11-05 09:28:29 +0100

    add link to player row

--- src/components/guildPage/GuildRosterRow.jsx
+++ src/components/guildPage/GuildRosterRow.jsx
@@ -1,11 +1,14 @@
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import DalApi from '../../dal/DalApi';
+
 import '../cssPages&Components/GuildPage.css';
 import '../cssPages&Components/ClassColor.css';
 
 const GuildRosterRow = (props) => {
   const {
-    // player,
+    region,
+    realm,
     player: {
       character: {
         level,
@@ -20,20 +23,25 @@ const GuildRosterRow = (props) => {
   const classe = DalApi.getClassesAndSpecsBySlug(slug);
   // console.log(player, classe);
   return (
-    <tr className="d-flex playerRow text-left align-items-center">
-      <td className="col-md-5 font-weight-bold">
-        <img
-          src={classe.image}
-          alt="class icon"
-          className="w-25 classImage pr-2"
-        />
-        <span className={classe.name.replace(' ', '')}>{name}</span>
-      </td>
-      <td className="col-md-3">{spec.name}</td>
-      <td className="col-md-2">{race.name}</td>
-      <td className="col-md-1">{level}</td>
-      <td className="col-md-1">{itemLevelEquipped}</td>
-    </tr>
+    <Link
+      to={`/player/${name}/${region}/${realm}/`}
+      style={{ textDecoration: 'none' }}
+    >
+      <tr className="d-flex playerRow text-left align-items-center">
+        <td className="col-md-5 font-weight-bold">
+          <img
+            src={classe.image}
+            alt="class icon"
+            className="w-25 classImage pr-2"
+          />
+          <span className={classe.name.replace(' ', '')}>{name}</span>
+        </td>
+        <td className="col-md-3">{spec.name}</td>
+        <td className="col-md-2">{race.name}</td>
+        <td className="col-md-1">{level}</td>
+        <td className="col-md-1">{itemLevelEquipped}</td>
+      </tr>
+    </Link>
   );
 };
 
@@ -42,6 +50,8 @@ export default GuildRosterRow;
 GuildRosterRow.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   player: propTypes.object.isRequired,
+  region: propTypes.string.isRequired,
+  realm: propTypes.string.isRequired,
   name: propTypes.string.isRequired,
   // slug: propTypes.string.isRequired,
 };
