e7df1a6d253b6594552f2f68aa3ca96eab18cd93
DavidDvpt <david.mosca69@gmail.com>
2020-11-10 15:42:47 +0100

    resolvs all proptyps bug on guildPage and childs

--- src/components/GuildRow.jsx
+++ src/components/GuildRow.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import FactionIcons from './flags/FactionIcons';
 
-function GuildRow({ name, faction, rank, region, realm }) {
+function GuildRow({ name, faction, rank, region, realm, slug }) {
   return (
-    <tr className="row" to={`/Guild/${region}/${realm}/${name}`}>
+    <tr className="row" to={`/Guild/${region}/${realm}/${slug}`}>
       <td className="col-2 d-flex align-items-center">{rank}</td>
       <th className="col-8 d-flex align-items-center">
         <strong style={{ fontSize: '21px' }}>{name}</strong>
@@ -21,6 +21,7 @@ GuildRow.propTypes = {
   rank: PropTypes.number.isRequired,
   region: PropTypes.string.isRequired,
   realm: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
 };
 
 export default GuildRow;
