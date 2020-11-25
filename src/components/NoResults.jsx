08b9272d957c5fa192bdb0384486c988847e1be5
David MOSCA <david.mosca69@gmail.com>
2020-11-25 09:26:53 +0100

    add content to component

---
+++ src/components/NoResults.jsx
@@ -0,0 +1,11 @@
+import Container from 'reactstrap';
+
+function NoResults() {
+  return (
+    <Container className="d-flex align-content-center">
+      <p className="text-center">No Results</p>
+    </Container>
+  );
+}
+
+export default NoResults;
